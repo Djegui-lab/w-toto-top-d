c444a5314e5a121f99fec3362c6e81aa2316db5f
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2024-11-16 01:16:22 +0100

    Update ok.py

--- ok.py
+++ ok.py
@@ -1,62 +1,14 @@
 import streamlit as st
-import pandas as pd
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
 
-# Fonction pour charger les données depuis Google Sheets
-def load_data(sheet_name="Sheet1"):
-    try:
-        # Définissez les autorisations et l'accès au fichier JSON de clé d'API
-        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-        credentials = ServiceAccountCredentials.from_json_keyfile_name("test-wague-9a205da3c6ca.json", scope)
+# Afficher un message de bienvenue
+st.write("Bonjour !")
 
-        # Authentification avec les informations d'identification
-        gc = gspread.authorize(credentials)
+# Demander à l'utilisateur son nom
+nom = st.text_input("Quel est ton nom ?")
 
-        # Ouvrir la feuille de calcul par son nom ou URL
-        worksheet = gc.open(sheet_name).sheet1
+# Si l'utilisateur a saisi son nom, l'afficher
+if nom:
+    st.write(f"Bonjour {nom} !")
 
-        # Lire les données de la feuille de calcul
-        data = worksheet.get_all_values()
-
-        # Convertir les données en un DataFrame pandas
-        df = pd.DataFrame(data[1:], columns=data[0])
-        return df
-
-    except Exception as e:
-        st.error(f"Erreur lors du chargement des données : {e}")
-        return None
-
-
-# Application principale Streamlit
-st.title("Application web pour l'analyse de données (en temps réel)")
-
-# Entrée pour sélectionner le nom de la feuille Google Sheets
-sheet_name = st.text_input("Entrez le nom de la feuille Google Sheets :", "message_de_suivis_devis")
-
-# Charger et afficher les données si le nom de la feuille est fourni
-if sheet_name:
-    data = load_data(sheet_name)
-
-    if data is not None:
-        st.write("Données chargées avec succès !")
-
-        # Afficher les données dans une table interactive
-        st.dataframe(data)
-
-        # Sélectionner une colonne à filtrer
-        if not data.empty:
-            column_to_filter = st.selectbox("Choisissez une colonne pour filtrer :", data.columns)
-
-            # Afficher les valeurs uniques de la colonne et permettre de filtrer
-            unique_values = data[column_to_filter].unique()
-            selected_value = st.selectbox("Choisissez une valeur à afficher :", unique_values)
-
-            # Filtrer les données et afficher les résultats
-            filtered_data = data[data[column_to_filter] == selected_value]
-            st.write(f"Données filtrées par {column_to_filter} = {selected_value} :", filtered_data)
-
-# Auteur
-st.write("AUTEUR : DJEGUI-WAUE")
 
 
